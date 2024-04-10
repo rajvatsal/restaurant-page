4fab312c324fec912a35bae5dcc8accf16024b24
Vatsal <rajvatsal2004@gmail.com>
2024-04-10 21:27:34 +0530

    feat(home): Create about us section in js

--- src/scripts/home.js
+++ src/scripts/home.js
@@ -23,6 +23,25 @@ function fillHeroSection(hero) {
 	hero.appendChild(image);
 }
 
+function fillAboutUs(aboutUs) {
+	const paraContent = `
+        At Chilliz, we believe that every meal should be a delightful journey
+        of flavors, crafted with passion and served with warmth. Nestled in
+        the heart of Dehradun, our restaurant is a culinary haven where
+        exquisite cuisine meets inviting ambiance. Our dedicated team of chefs
+        brings together a fusion of traditional recipes and innovative
+        techniques, sourcing the finest ingredients to create dishes that
+        tantalize the palate. Whether you're joining us for a casual brunch, a
+        romantic dinner, or a celebration with loved ones, we strive to
+        provide an unforgettable dining experience. At chilliz, we're not just
+        about serving food; we're about creating memories and fostering
+        connections. Welcome to our table; let us enchant you with our
+        culinary delights
+      `;
+	aboutUs.appendChild(createElement("h2", "", "", "About us"));
+	aboutUs.appendChild(createElement("p", "", "", paraContent));
+}
+
 export default function home() {
 	const content = document.querySelector("div#content");
 	const sections = [
@@ -33,6 +52,6 @@ export default function home() {
 	];
 
 	fillHeroSection(sections[0]);
-	content.prepend(sections[0]);
-	// sections.forEach((section) => content.appendChild(section));
+	fillAboutUs(sections[1]);
+	sections.forEach((section) => content.appendChild(section));
 }
